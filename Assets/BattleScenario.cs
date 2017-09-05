bdaa7aabe014b82551bd0088533985cd63a8666d
JayReese <reese.j.jordon@gmail.com>
2017-09-05 18:50:19 -0500

    INCOMPLETE. Battle scenario class started.

---
+++ Assets/BattleScenario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleScenario : MonoBehaviour
+{
+    public float BaseSuccessRate { get; private set; }
+
+    #region Testing.
+    float teamAbility, enemyAbility;
+    #endregion
+
+    // Use this for initialization
+    void Start ()
+    {
+        teamAbility = 19;
+        enemyAbility = 30;
+
+        Debug.Log(teamAbility / enemyAbility);
+
+        BaseSuccessRate = (int)Mathf.Floor( 50 * (teamAbility / enemyAbility) );
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
